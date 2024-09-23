af9b4412fd0a51f14f763c352c05814cf9529cb5
Hiru <malov.danila@corementa.ru>
2024-09-23 17:48:38 +0300

    Remade contacts styles

--- resources/views/auth/contacts.blade.php
+++ resources/views/auth/contacts.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex flex-col justify-start h-[800px] mx-10 p-10 rounded-[138px] shadow-[0px_14px_34.1px_0px_#00000040;] text-center">
+    <div class="flex flex-col justify-start h-[650px] w-full mx-10 p-10 rounded-[138px] shadow-[0px_14px_34.1px_0px_#00000040;] text-center">
         <h1 class="text-[96px] font-bold uppercase">Контакты</h1>
         <div class="h-full flex justify-between items-center -translate-y-32">
             <img src="{{ asset('img/logo_transparent.png') }}" alt="Logo" class="-translate-x-10 transition-all duration-200 ease-in-out hover:rotate-12">
