fa97e73f83c9dc33727ab1885a057d729366d51d
Hiru <malov.danila@corementa.ru>
2024-10-12 10:17:07 +0300

    update nodes

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'e201392fa67f128f26e97cfcc87e9be479d1e585',
+        'reference' => 'cc73c81376c75ddbcb920f06ae48df64efb6c0bc',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -388,7 +388,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'e201392fa67f128f26e97cfcc87e9be479d1e585',
+            'reference' => 'cc73c81376c75ddbcb920f06ae48df64efb6c0bc',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -754,8 +754,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
