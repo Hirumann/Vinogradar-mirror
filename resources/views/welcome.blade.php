3f15c0a56393b2b59bb11ee01489b5673ca10a65
Hook-And <osya_stalin_1878@mail.ru>
2024-10-18 05:26:24 +0300

    small fixes for welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,10 @@
 </head>
 <body style="background: url({{ asset('img/Bg_Img.svg') }}) no-repeat left center / contain fixed;">
     <div class="flex flex-row-reverse mt-10 mr-20">
-        <a class="w-48 h-16 bg-[#4b4b4b] rounded-full flex justify-center items-center text-xl text-white font-bold border-none transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-neutral-800 duration-300" 
-            href="{{ route('login') }}"> <img class="mr-4" src="{{ asset('img/Icon_Acc.svg') }}" alt="Icon_Acc">войти
+        <a class="w-48 h-16 px-6 bg-[#4b4b4b] rounded-full flex justify-between items-center text-xl text-white font-bold border-none transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-neutral-800 duration-300"
+            href="{{ route('login') }}">
+            <img src="{{ asset('img/Icon_Acc.svg') }}" alt="Icon_Acc">
+            <span class="leading-5 uppercase mr-2">Войти</span>
         </a>
     </div>
     <div class="w-full flex mx-auto">
@@ -21,16 +23,30 @@
         </div>
         <div class="flex flex-col justify-evenly font-bold font-fontProjects w-1/2">
             <h1 class="w-full text-[150px] leading-[180px]">Цифровой Виноградник</h1>
-            <div class="w-full columns-2 text-6xl flex flex-wrap gap-y-8">
-                <span class="w-1/2 flex items-center"><img class="w-20 h-20 mr-2" src="{{ asset('img/Icon_History.svg') }}" alt="Icon_History">история поля</span>
-                <span class="w-1/2 flex items-center"><img class="w-20 h-20 mr-2" src="{{ asset('img/Icon_Journal.svg') }}" alt="Icon_Journal">агрожурнал</span>
-                <span class="w-1/2 flex items-center"><img class="w-20 h-20 mr-2" src="{{ asset('img/Icon_Weather.svg') }}" alt="Icon_Weather">погода</span>
-                <span class="w-1/2 flex items-center"><img class="w-20 h-20 mr-2" src="{{ asset('img/Icon_Recomendation.svg') }}" alt="Icon_Recomendation">рекомендации</span>
+            <div class="w-full columns-2 text-6xl flex flex-wrap gap-y-8 font-normal">
+                <div class="w-1/2 flex items-center">
+                    <img class="w-20 h-20 mr-2" src="{{ asset('img/Icon_History.svg') }}" alt="Icon_History">
+                    <span>история поля</span>
+                </div>
+                <div class="w-1/2 flex items-center">
+                    <img class="w-20 h-20 mr-2" src="{{ asset('img/Icon_Journal.svg') }}" alt="Icon_Journal">
+                    <span>агрожурнал</span>
+                </div>
+                <div class="w-1/2 flex items-center">
+                    <img class="w-20 h-20 mr-2" src="{{ asset('img/Icon_Weather.svg') }}" alt="Icon_Weather">
+                    <span>погода</span>
+                </div>
+                <div class="w-1/2 flex items-center">
+                    <img class="w-20 h-20 mr-2" src="{{ asset('img/Icon_Recomendation.svg') }}" alt="Icon_Recomendation">
+                    <span>рекомендации</span>
+                </div>
             </div>
         </div>
     </div>
     <footer class="w-full flex justify-center items-center">
-        <a href="https://fasie.ru/"><img src="{{ asset('img/fckthslg.png') }}" alt="Link"></a>
+        <a href="https://fasie.ru/">
+            <img src="{{ asset('img/fckthslg.png') }}" alt="Link" style="height: 108px">
+        </a>
     </footer>
 </body>
 </html>
